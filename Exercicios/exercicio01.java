9689e09d6057fe03c82b9d30bbfe942cc75b0b44
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-16 22:00:56 -0300

    feat: Implementado exemplo de variável

---
+++ Exercicios/exercicio01.java
@@ -0,0 +1,6 @@
+public class exercicio01 {
+    public static void main(String[] args){
+        int farinha = 2;
+        System.out.println("A receita requer "+ farinha + " xícaras de farinha");
+    }
+} 
