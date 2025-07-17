82dde4c5f05ad3d4496625dd9a748c26191d43ef
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-17 20:13:10 -0300

    feat: Implementado exercício de declaração de variáveis e concatenação

---
+++ Exercicios/exercicio04.java
@@ -0,0 +1,8 @@
+public class exercicio04 {
+    public static void main(String[] args) {
+        int quantidadeFarinha = 5;
+        int quantidadeAcucar = 10;
+
+        System.out.println("Na padaria nós temos "+ quantidadeFarinha + " sacos de farinha e " + quantidadeAcucar + " sacos de açúcar");
+    }
+}
