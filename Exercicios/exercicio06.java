745d84441e0a241d27dca5e9ac7684e732d5ef84
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-17 20:26:39 -0300

    feat: Implementado exercício de operações com diferentes tipos de variáveis

---
+++ Exercicios/exercicio06.java
@@ -0,0 +1,20 @@
+public class exercicio06 {
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 1000;
+        char c = 'a';
+        int d = 12345;
+        long e = 700000000;
+        float f = 1.5f;
+        double g = 555.55;
+
+
+        var soma = a + b;
+        var multiplicacao = f * d;
+        var subtracao = b - a;
+        var divisao = e / f;
+        System.out.println("O char é: " + c);
+        System.out.println("A Soma, Multiplicação, Subtração e Divisão São respectivamente: "
+         + soma + ' '  + multiplicacao + ' ' + subtracao + ' ' + divisao);
+    }
+}
