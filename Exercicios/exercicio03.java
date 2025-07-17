27829a0f004fc3504c3ead913b26d0d1b06e57af
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-17 17:21:33 -0300

    feat: Adicionados exercícios de promoções automáticas

---
+++ Exercicios/exercicio03.java
@@ -0,0 +1,29 @@
+public class exercicio03 {
+    public static void main(String[] args){
+        byte a = 10;
+        int b = a + 5;
+
+        System.out.println("Valor de a = " + a);
+
+        int c = 10;
+        float d = 1.5f;
+        float e = a*b;
+
+        System.out.println("Valor de c vezes d = " + e);
+
+
+        int f = 10;
+        double g = 1.5;
+        double h = f * g; 
+
+        System.out.println("Valor de f vezes g = " + h);
+
+        int i = 10;
+        float j = 1.5f;
+        float k = i * j;
+
+        System.out.println("Valor de i vezes j = " + k);
+
+
+    }
+}
