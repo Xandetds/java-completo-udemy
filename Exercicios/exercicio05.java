448ed04a1a5b51435e8adb7d5fbffbe0582bec08
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-17 20:17:04 -0300

    feat: Implementado exercício de cálculo de área com variáveis decimais

---
+++ Exercicios/exercicio05.java
@@ -0,0 +1,9 @@
+public class exercicio05 {
+    public static void main(String[] args) {
+        double largura = 5.5d;
+        double altura = 7.9;
+
+        var area = largura * altura;
+        System.out.println("A area é: " + area);
+    }
+}
