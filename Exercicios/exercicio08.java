7c2e002311b2af398872298fc555c464f6d46214
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-18 16:55:43 -0300

    feat: Implementado exercício de operadores unários

---
+++ Exercicios/exercicio08.java
@@ -0,0 +1,16 @@
+public class exercicio08 {
+    public static void main(String[] args) {
+        var contador = 1;
+        contador += 1;
+        System.out.println("O valor do contador é: " + contador);
+
+        var numero = 5;
+        var numero2 = 10;
+        numero++;
+        ++numero;
+        --numero2;
+        numero2--;
+
+        System.out.println(numero + " " + numero2 );
+    }
+}
