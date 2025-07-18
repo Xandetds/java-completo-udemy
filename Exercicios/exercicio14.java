6da497c4c2590b0321f56a8c6b0f623c4e50d1bf
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-18 20:12:23 -0300

    feat: Implementado exercício de cálculo de média e condição de aprovação

---
+++ Exercicios/exercicio14.java
@@ -0,0 +1,12 @@
+public class exercicio14 {
+    public static void main(String[] args) {
+        int nota1 = 3;
+        int nota2 = 8;
+        if ((nota1 + nota2)/2 == 6){
+            System.out.println("Aprovado");
+        }
+        else{
+            System.out.println("Reprovado");
+        }
+    }
+}
