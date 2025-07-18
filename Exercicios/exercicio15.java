f29e57a45c102231d3edcbd549aea19ec4ea96d2
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-18 20:21:50 -0300

    feat: Implementado exercício de condição de aprovação com frequência

---
+++ Exercicios/exercicio15.java
@@ -0,0 +1,13 @@
+public class exercicio15 {
+    public static void main(String[] args) {
+        int nota1 = 5;
+        int nota2 = 8;
+        var frequencia = 75;
+        if (((nota1 + nota2)/2 == 6) && (frequencia >= 75)){
+            System.out.println("Aprovado");
+        }
+        else{
+            System.out.println("Reprovado");
+        };
+    }
+}
