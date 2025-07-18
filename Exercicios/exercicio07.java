70c2846c146064b75f890997b0cf74335713d3f6
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-18 16:48:00 -0300

    feat: Implementado exercício de operadores de atribuição

---
+++ Exercicios/exercicio07.java
@@ -0,0 +1,18 @@
+public class exercicio07{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        var c = a + b;
+
+        System.out.println("O valor da adição é de " + c);
+
+        var numero = 5;
+        var soma = numero += 3;
+        var subtracao = numero -= 3;
+        var multiplicacao = numero *= 3;
+
+
+        System.out.println("Os valores são "+ soma + ' ' + subtracao + ' ' + multiplicacao);
+
+    }
+}
