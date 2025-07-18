e8c90878359d902d7e64da1e6787eea834d3921c
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-18 18:55:21 -0300

    feat: Implementado exercício de condicionais e operador ternário

---
+++ Exercicios/exercicio09.java
@@ -0,0 +1,23 @@
+public class exercicio09{
+    public static void main(String[] args) {
+        var numero = 10;
+
+        if (numero % 2 == 0){
+            System.out.println("O número " + numero + " é par");
+        }
+        else{
+            System.out.println("O número " + numero + " é impar");
+        };
+
+
+        int numero2 = 9;
+        String resultado = (numero2 % 2 == 0) ? "par" : "impar";
+        System.out.println(resultado);
+ 
+
+        Object obj = "alexandre";
+        if (obj instanceof String) {
+        System.out.println(obj); 
+        } 
+    }
+}
