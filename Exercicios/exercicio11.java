736ed52836090f95ef1678b542cd1b8757244e32
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-18 19:32:49 -0300

    feat: Implementado exercício de estrutura switch nas versões antiga e nova

---
+++ Exercicios/exercicio11.java
@@ -0,0 +1,36 @@
+public class exercicio11 {
+    public static void main(String[] args) {
+        var nivel = 1;
+
+        switch(nivel) {
+            case 1 -> System.out.println("Insatisfeito");
+            case 2 -> System.out.println("Satisfeito");
+            case 3 -> System.out.println("Muito satisfeito");
+            default -> System.out.println("ERRO");
+        };
+
+        int diaDaSemana = 3;
+        switch (diaDaSemana) {
+            case 1:
+            System.out.println("Seg");
+            break;
+            case 2:
+            System.out.println("Ter");
+            break;
+            case 3:
+            System.out.println("Qua");
+            break;
+            case 4:
+            System.out.println("Qui");
+            break;
+            case 5:
+            System.out.println("Sex");
+            break;
+            default:
+            System.out.println("Dia desconhecido");
+            break;
+            };
+                    
+    
+}
+}
