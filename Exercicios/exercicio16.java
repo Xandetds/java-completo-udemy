8e8f3a266b560354b156497265ca71641e179045
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-20 14:37:21 -0300

    feat: Implementado exercício de estruturas de repetição (for, while, do-while)

---
+++ Exercicios/exercicio16.java
@@ -0,0 +1,24 @@
+public class exercicio16 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+        System.out.println(" ");
+
+        int j = 2;
+
+        while (j < 5){
+            System.out.println(j);
+            j++;
+        }
+        System.out.println(" ");
+
+        
+        int k = 20;
+
+        do {
+            System.out.println(k);
+            k++;
+        } while(k < 5);
+}
+}
