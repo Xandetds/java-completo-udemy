5e14e1d24fdcd7663f5ee4dc36a470c1f079fb16
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-20 16:49:09 -0300

    feat: Implementado exercício de soma regressiva com do-while

---
+++ Exercicios/exercicio20.java
@@ -0,0 +1,12 @@
+public class exercicio20 {
+    public static void main(String[] args) {
+        var soma = 0;
+        var numero = 10;
+        do{
+            soma += numero;
+            numero--;
+            System.out.println(soma);
+        }while (numero > 0);
+        System.out.println(soma);
+    }
+}
