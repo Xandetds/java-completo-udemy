697532e09ea6dc0852d754b74d22917db3dc7ede
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-20 14:45:27 -0300

    feat: Implementado exercício de controle de loop (break e continue)

---
+++ Exercicios/exercicio17.java
@@ -0,0 +1,18 @@
+public class exercicio17 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+            break; 
+            }
+            System.out.println(i);
+            } 
+            System.out.println(" ");
+
+        for (int j = 0; j < 10; j++) {
+            if (j == 5) {
+            continue; 
+            }
+            System.out.println(j);
+            } 
+    }
+}
