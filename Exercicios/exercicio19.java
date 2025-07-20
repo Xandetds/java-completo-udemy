206cc60daf2d0b055f3abf311e72d6a55f252ebc
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-20 16:41:42 -0300

    feat: Implementado exercício de loop while para contagem

---
+++ Exercicios/exercicio19.java
@@ -0,0 +1,11 @@
+public class exercicio19 {
+    public static void main(String[] args) {
+        int contadorDoces = 1;
+        while (contadorDoces <= 3) {
+            System.out.println("Filho comeu o doce número: " + contadorDoces);
+            contadorDoces++;
+        }
+        System.out.println("A mãe disse para parar de comer");
+
+    }
+}
