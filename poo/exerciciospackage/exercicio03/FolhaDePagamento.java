7d2a3631298f1bb210488ea0909483c82731c2f0
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-22 14:17:09 -0300

    feat: Adicionado exercício de uso do modificador protected e visibilidade de pacotes

---
+++ poo/exerciciospackage/exercicio03/FolhaDePagamento.java
@@ -0,0 +1,7 @@
+package poo.exerciciospackage.exercicio03;
+
+public class FolhaDePagamento {
+    public static void main(String[] args) {
+        testePackage tp = new testePackage();
+    }
+}
---
+++ poo/exerciciospackage/exercicio03/testePackage.java
@@ -0,0 +1,7 @@
+package poo.exerciciospackage.exercicio03;
+
+public class testePackage {
+    public testePackage() {
+        Funcionario2 funcionario2 = new Funcionario2();
+    }
+}
