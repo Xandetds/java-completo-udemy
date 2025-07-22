043f6cbbd777434352cefc2ecbaed06fa4d8c392
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-21 22:18:00 -0300

    feat: adiciona exercicio01 com classes Funcionario e FolhaDePagamento usando package

---
+++ poo/exerciciospackage/exercicio01/FolhaDePagamento.java
@@ -0,0 +1,8 @@
+package poo.exerciciospackage.exercicio01;
+import poo.exerciciospackage.exercicio01.Funcionario;
+
+public class FolhaDePagamento {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Maria", 6200.00);
+    }
+}
---
+++ poo/exerciciospackage/exercicio01/Funcionario.java
@@ -0,0 +1,13 @@
+package poo.exerciciospackage.exercicio01;
+
+public class Funcionario {
+    String nome;
+    double salario;
+
+    public Funcionario(String nomeInit, double salarioInit) {
+        nome = nomeInit;
+        salario = salarioInit;
+        System.out.println("Nome: " + nome);
+        System.out.println("Salario: " + salario);
+    }
+}
