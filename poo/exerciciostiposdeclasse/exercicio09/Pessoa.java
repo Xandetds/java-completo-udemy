365d4af9946ee8fc6351775c954a34be3ccb0bd9
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-23 09:11:42 -0300

    feat: Adicionado exercício de modificadores de acesso e métodos Get/Set

---
+++ poo/exerciciostiposdeclasse/exercicio09/Pessoa.java
@@ -0,0 +1,17 @@
+package poo.exerciciostiposdeclasse.exercicio09;
+public class Pessoa{
+    public String nome;
+    private int idade;
+    protected String endereco;
+    long telefone;
+
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+        }            
+
+}
