fe1cda495de53e134ad83f209f83461b765d40a8
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-31 20:23:47 -0300

    feat: Adicionado exercício de iteração tradicional para comparação futura com Lambda

---
+++ ProgramacaoFuncional/expressoes_lambda/exercicio01.java
@@ -0,0 +1,13 @@
+package ProgramacaoFuncional.expressoes_lambda;
+
+import java.util.*;
+
+public class exercicio01{
+    public static void main(String[]args){
+        List<Integer> listaDeNumeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        for (Integer elemento : listaDeNumeros){
+            System.out.println(elemento);
+        }
+    }
+}
