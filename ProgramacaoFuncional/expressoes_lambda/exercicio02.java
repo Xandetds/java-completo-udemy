d6645841f7f288f9f5d884a8f927a2fe85e6c661
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-31 20:36:02 -0300

    feat: Adicionado exercício de iteração com expressão lambda forEach

---
+++ ProgramacaoFuncional/expressoes_lambda/exercicio02.java
@@ -0,0 +1,12 @@
+package ProgramacaoFuncional.expressoes_lambda;
+
+import java.util.*;
+
+public class exercicio02{
+    public static void main(String[] args) {
+
+    List<Integer> listaDeNumeros = Arrays.asList(1, 2, 3, 4, 5);
+
+    listaDeNumeros.forEach( (Integer numero) -> System.out.println(numero));
+}
+}
