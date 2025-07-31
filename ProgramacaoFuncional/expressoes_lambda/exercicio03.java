cf37808d39c5b0c68dbade870b2712f31109a3c6
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-31 20:50:18 -0300

    feat: Adicionado exercício de forEach com expressão lambda e condicional

---
+++ ProgramacaoFuncional/expressoes_lambda/exercicio03.java
@@ -0,0 +1,19 @@
+package ProgramacaoFuncional.expressoes_lambda;
+
+import java.util.*;
+
+public class exercicio03{
+    public static void main(String[] args) {
+
+    List<Integer> listaDeNumeros = Arrays.asList(1, 2, 3, 4, 5, 100, 200);
+    List<Integer> numerosPares = new ArrayList<>();
+
+    listaDeNumeros.forEach( numero -> {
+        if (numero % 2 == 0){
+            numerosPares.add(numero);
+        }
+        });
+    
+    System.out.println("Numeros pares: " + numerosPares);
+    }
+}
