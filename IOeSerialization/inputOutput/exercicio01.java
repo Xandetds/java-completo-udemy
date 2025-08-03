f7d1ab0bb111c707cf8f3e71ab933e340d73e634
Alexandre <alexandretibes9@gmail.com>
2025-08-03 18:10:26 -0300

    feat: Adicionado exercício de conversão de tipos (int para char)

---
+++ IOeSerialization/inputOutput/exercicio01.java
@@ -0,0 +1,15 @@
+package IOeSerialization.inputOutput;
+
+public class exercicio01 {
+    public static void main(String []args){
+        int num1 = 79;
+        int num2 = 108;
+        int num3 = 225;
+
+        char letra1 = (char) num1;
+        char letra2 = (char) num2;
+        char letra3 = (char) num3;
+
+        System.out.println("Saudacao: " + letra1 + letra2 + letra3);
+    }
+}
